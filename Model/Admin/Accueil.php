60dfa06e8d38210e110930bd7e19bbb6415bb612
francois <francoismazerbourg@gmail.com>
2023-03-16 14:55:28 +0100

    html page accueil et création du dossier style

---
+++ Model/Admin/Accueil.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <main class="body">
+        <h1><span class="title_accueil"> Trouvez l'appartement de vos rêves !</span></h1>
+
+        <div class='sec1'>
+
+            <span>+2000</span> de nos agents pour vous accompagner dans vos projets
+        </div>
+
+        <div class='sec2'>
+            icon 
+
+            <span>92%</span> de clients satisfaits
+            <div>
+                icon France
+
+                une expertise partout en France
+            </div>
+        </div>
+
+        <div class='sec3'>
+            Nous recrutons des collaborateurs, en agence et au siège
+            <button>Nous rejoindre</button>
+        </div>
+    </main>
+</body>
+</html>
