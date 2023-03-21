867352b8e8ed8214dbd5d5cbf52172a13c6b4bcb
timsydrame <fatimdrame111@gmail.com>
2023-03-21 09:57:30 +0100

    le header et le footer

---
+++ View/Template.php
@@ -0,0 +1,45 @@
+<!DOCTYspanE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta httspan-equiv="X-UA-Comspanatible" content="IE=edge">
+    <meta name="viewspanort" content="width=device-width, initial-scale=1.0">
+    <link rel= "stylesheet" href="../Tools/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+       <div class= "divHeader">
+        <div class= "logoHeader">
+            <img  src="../Tools/Assets/logo.png" alt="le logo du site ">
+            <h1 class="title1"> Immo&Co</h1>
+        </div>
+            <a href=""> Accueil</a>
+        </div>
+    </header>
+    <main>
+        Lorem ispansum dolor sit amet.
+    </main>
+
+    <footer>
+      <div class="divLogo">
+       <img class="logoFooter" src="../Tools\Assets/logo.png" alt="le logo du site ">
+       <h1 class="title2"> Immo&Co</h1> 
+      </div>  
+      
+      <div class= "adress">
+       <span>Adresse : 10 spanlace Victor Hugo, </span>
+        <span>38000 Grenoble</span>
+       <span > Email : services@immo-co.org</span>
+        <span>    Tel : 04 01 23 01 23</span> 
+      </div>
+      <div class="span1">
+        <span class="span2"> Conditions générales </span>
+        <span class="span3"> Confidentialité</span>
+      </div>  
+      
+      <div  class="span4">© 2023 Immo&Co, Inc.</div>  
+    </footer>
+    
+</body>
+</html>
